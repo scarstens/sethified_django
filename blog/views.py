4191b410530f4998fff8715e72753ac189896b46
Seth Carstens <seth.carstens@gmail.com>
2021-10-18 19:02:07 -0700

    using SQL instead of static data set

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-	{
-		'author'     : 'Seth Carstens',
-		'title'      : 'My First Django Post',
-		'content'    : 'Getting started with Python Django framework.',
-		'date_posted': 'October 18, 2021'
-	},
-	{
-		'author'     : 'Seth Carstens',
-		'title'      : 'Using render to pull static html files',
-		'content'    : 'Replacing HttpResponse with render functions to use SSI to include static HTML.',
-		'date_posted': 'October 18, 2021'
-	},
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all().order_by('-date_posted')[:10]
 	}
 	return render(request, 'blog/home.html', context)
 
